c240ebac37232957f341c99227c84bf1daf3fadf
jdruwe <druwe.jeroen@gmail.com>
2019-05-19 17:27:14 +0200

    Fixed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAy4u1kHqbTrftc4C3E0uKoLA3RN3TIf2A',
+    authDomain: 'bachelor-party-pokedex.firebaseapp.com',
+    databaseURL: 'https://bachelor-party-pokedex.firebaseio.com',
+    projectId: 'bachelor-party-pokedex',
+    storageBucket: 'bachelor-party-pokedex.appspot.com',
+    messagingSenderId: '172025890254'
+  }
 };
